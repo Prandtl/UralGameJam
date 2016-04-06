3039c70645c5810717675b21a526c82c63c211dd
Kek <tarasovki94@gmail.com>
2016-04-06 22:10:19 +0500

    FirstPersonController mouse movement pitch update

--- TheOtherSide/Assets/_Scripts/FirstPersonController.cs
+++ TheOtherSide/Assets/_Scripts/FirstPersonController.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class FirstPersonController : MonoBehaviour {
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
     // Use this for initialization
     void Start () {
 	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		MoveOnWasd();
+		LookWithMouse();
 	}
 
     void MoveOnWasd ()
@@ -30,15 +33,19 @@ public class FirstPersonController : MonoBehaviour {
     {
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
+		if (pitch > maxPitch)
+			pitch = maxPitch;
+		if (pitch < minPitch)
+			pitch = minPitch;
         // turn character (z axis only)
         transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
         // turn mainCamera
-        Camera.main.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);  // turn mainCamera
+        Camera.main.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        MoveOnWasd();
-        LookWithMouse();
-	}
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+	private float maxPitch = 90.0f;
+	private float minPitch = -90.0f;
+
 }

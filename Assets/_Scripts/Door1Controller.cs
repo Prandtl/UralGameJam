dc6c0308569b71fdfcd587ec29ea94f2ca067964
Kek <tarasovki94@gmail.com>
2016-04-09 04:34:02 +0500

    Door1Controller initial

---
+++ Assets/_Scripts/Door1Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door1Controller : MonoBehaviour {
+
+	public float maxOpeningLimit;
+
+	// Use this for initialization
+	void Start () {
+		leftDoor = transform.Find ("LeftDoor");
+		rightDoor = transform.Find ("RightDoor");
+		initialPosition = leftDoor.transform.position;
+	}
+
+	private Transform leftDoor;
+	private Transform rightDoor;
+	private Vector3 initialPosition;
+
+	// Update is called once per frame
+	void Update () {
+		Open ();
+	}
+
+	void Open () {
+	}
+
+	void Close () {
+
+	}
+}

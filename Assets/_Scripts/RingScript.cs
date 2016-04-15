33af936b38a321dd4ed20d919ec694e8d484f2b2
Prandtl <prandtl211@gmail.com>
2016-04-16 00:27:10 +0500

    Большой заливон говна

---
+++ Assets/_Scripts/RingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class RingScript : MonoBehaviour, IUsable {
+
+	AudioSource src;
+
+	public void Use(){
+		src.Play ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		src = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

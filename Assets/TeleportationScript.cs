3806bbf065601f1dbf1988f2ab32f74ea7128053
Prandtl <prandtl211@gmail.com>
2016-04-16 15:56:58 +0500

    сделал телепорт

---
+++ Assets/TeleportationScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportationScript : MonoBehaviour {
+
+	public Vector3 PositionForTeleportation;	
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.transform.position = PositionForTeleportation;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

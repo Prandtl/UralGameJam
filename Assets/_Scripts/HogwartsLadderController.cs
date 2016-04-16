4a048be30efb6296432e209fe82b1ad880c0e4cb
Kek <tarasovki94@gmail.com>
2016-04-17 01:51:50 +0500

    Hog updated

---
+++ Assets/_Scripts/HogwartsLadderController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HogwartsLadderController : MonoBehaviour {
+
+	public float timeInterval;
+
+	// Use this for initialization
+	void Start () {
+		timeInterval = 10;
+		ladderSegments = gameObject.GetComponentsInChildren<LadderSegmentController>();
+		InvokeRepeating ("moveLadderSegments", 0, timeInterval);
+	}
+
+	private Component[] ladderSegments;
+
+	void Update () {
+		
+	}
+
+	void MoveLadderSegmentRandom (Component ladderSegment) {
+		int randomState = Random.Range (-1, 2);
+		ladderSegment.SendMessage("setState", randomState);
+	}
+
+	void moveLadderSegments () {
+		foreach( var ladderSegment in ladderSegments ) {
+			MoveLadderSegmentRandom(ladderSegment);	
+		}
+	}
+}

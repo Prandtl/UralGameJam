50612c29bb4371dca666a0e2f67e4b24a810f21a
Prandtl <prandtl211@gmail.com>
2016-04-17 09:48:24 +0500

    СТартовая сцена

--- Assets/_Scripts/DarkeningOnTrigger.cs
+++ Assets/_Scripts/DarkeningOnTrigger.cs
@@ -1,17 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DarkeningOnTrigger : MonoBehaviour {
+
+	public Light whiteLight;
+
+	public Image energyStar;
+
+	public AudioSource glitch;
+	public AudioSource turnoff;
+	public AudioSource turnon;
+	public GameObject lighthouse;
+	public List<Text> texts;
+	public List<float> intervals;
 
 	// Use this for initialization
 	void OnTriggerEnter(Collider other)
 	{
+		whiteLight.enabled = true;
 		RenderSettings.ambientIntensity = 0;
 		var player = Object.FindObjectOfType<PrandtlyPlayerController> ();
 		player.SetMoveState (false);
 		player.SetWatchState (false);
-		Invoke ("ChangeLevel", 5.0f);
+		player.transform.LookAt (lighthouse.transform);
+		glitch.Play ();
+		Invoke ("Darkness", 0.1f);
+		Invoke ("Turnoff", 2.0f);
+	}
+
+	void Darkness(){
+		whiteLight.enabled = false;
+	}
+
+	void Turnoff()
+	{
+		turnoff.Play ();
+		Invoke ("BootScreen", 2.0f);
+	}
+
+	void BootScreen(){
+		turnon.Play ();
+		energyStar.enabled = true;
+		StartCoroutine (ShowText ());
+		Invoke ("ChangeLevel", 7.0f);
+	}
+
+	IEnumerator ShowText(){
+		for (int i = 0; i < texts.Count; i++) {
+			texts [i].enabled = true;
+			var interval = i >= intervals.Count ? 1 : intervals [i];
+			yield return new WaitForSeconds (interval);
+		}
 	}
 
 	void ChangeLevel()

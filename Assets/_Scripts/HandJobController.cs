d6ec1bcdef34024a66cbbf929d0ae2675fbb3aa5
Kek <tarasovki94@gmail.com>
2016-04-17 12:00:52 +0500

    HandJob

---
+++ Assets/_Scripts/HandJobController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandJobController : MonoBehaviour {
+
+	public bool handSwitch;
+
+	// Use this for initialization
+	void Start () {
+		hand = transform.Find ("HandWrapper");
+		handSwitch = false;
+	}
+
+	private Transform hand;
+	
+	// Update is called once per frame
+	void Update () {
+		ControlHand ();
+		Debug.Log (hand.localEulerAngles);
+	}
+
+	void ControlHand() {
+		if (handSwitch)
+			HandDown ();
+		else
+			HandUp ();
+	}
+
+	void HandUp () {
+		if (hand.localEulerAngles.x < 360 && hand.localEulerAngles.x > 90)
+			hand.Rotate (Vector3.right);
+	}
+
+	void HandDown () {
+		if (hand.localEulerAngles.x > 360-90 && hand.localEulerAngles.y < 90)	//	Koctyl
+			hand.Rotate (Vector3.left);
+	}
+
+	void SwitchHand() {
+		handSwitch = !handSwitch;
+	}
+}

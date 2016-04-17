08a06a6d06757f674921db3e61c48b7215e3474f
Kek <tarasovki94@gmail.com>
2016-04-17 22:57:25 +0500

    kek

---
+++ Assets/_Scripts/IntegralStuffController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntegralStuffController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		failTexture1 = Resources.Load ("youAreStone") as Texture;
+		monitor = transform.Find ("Monitor");
+	}
+
+	private Texture failTexture1;
+	private Texture failTexture2;
+	private Texture failTexture3;
+	private Texture winTexture;
+
+	private Transform monitor;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

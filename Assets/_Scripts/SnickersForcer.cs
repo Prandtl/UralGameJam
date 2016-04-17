f8bc5c0578db28f4e5c5989f05a038e1fa391a69
Kek <tarasovki94@gmail.com>
2016-04-17 06:54:37 +0500

    ajksdhajs

---
+++ Assets/_Scripts/SnickersForcer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnickersForcer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	private Rigidbody rb;
+
+	void Force() {
+		rb.AddForce (transform.forward);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/_Scripts/VendingMachineController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class VendingMachineController : MonoBehaviour, IUsable {
+
+	public float speed;
+	public bool used;
+	public float maxOpeningLimit;
+
+	// Use this for initialization
+	void Start () {
+		shelf = transform.Find ("Shelf");
+		used = false;
+		speed = 100.0f;
+		maxOpeningLimit = 60.0f;
+		snickers = GetComponentInChildren<SnickersForcer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ControlShelf ();
+	}
+
+	void ControlShelf() {
+		if(used)
+			GetFood ();
+	}
+
+	private Component snickers;
+
+	void GetFood () {
+		if (shelf.localEulerAngles.z < 90 || shelf.localEulerAngles.z > 360 - maxOpeningLimit) {
+			shelf.Rotate (Vector3.back * speed * Time.deltaTime);
+		}
+	}
+
+	private Transform shelf;
+
+	public void Use () {
+		used = true;
+		snickers.SendMessage("Force");
+	}
+}
